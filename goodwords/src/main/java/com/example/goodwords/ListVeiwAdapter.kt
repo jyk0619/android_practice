d6b8c1b9881845ea5edf4288c15ac0ecda12f3c4
jyk0619 <jyk990619@naver.com>
2023-09-21 14:39:42 +0900

    오류수정

--- goodwords/src/main/java/com/example/goodwords/ListVeiwAdapter.kt
+++ goodwords/src/main/java/com/example/goodwords/ListVeiwAdapter.kt
@@ -6,27 +6,29 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 
-class ListVeiwAdapter (val List: MutableList<String>): BaseAdapter(){
+class ListVeiwAdapter (val list : MutableList<String>): BaseAdapter(){
     override fun getCount(): Int {
-        return List.size
+        return list.size
     }
 
-    override fun getItem(p0: Int): Any {
-        return List[position]
+    override fun getItem(position: Int): Any {
+        return list[position]
     }
 
-    override fun getItemId(p0: Int): Long {
+    override fun getItemId(position: Int): Long {
         return position.toLong()
     }
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+    override fun getView(position: Int,convertView: View?, parent: ViewGroup?): View {
         var convertView = convertView
         if(convertView==null){
-            convertView=LayoutInflater.from(parent.context).inflate(R.layout.listview_item,parent,false)
+            if (parent != null) {
+                convertView=LayoutInflater.from(parent.context).inflate(R.layout.listview_item,parent,false)
+            }
 
         }
-        val listviewText=convertView.findViewById<TextView>(R.id.listViewTextArea)
-        listviewText!!.text = List[position]
-        return convertView
+        val listviewText= convertView?.findViewById<TextView>(R.id.listViewTextArea)
+        listviewText!!.text = list[position]
+        return convertView!!
     }
 }
