3f4f0b71c54d98dfec9c13d116e19e270f9a2d88
jyk0619 <jyk990619@naver.com>
2023-09-21 17:03:28 +0900

    트로트 실습

---
+++ trot_list/src/main/java/com/example/trot_list/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.trot_list
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1.navigation
+
+        //2.fragment
+
+        //3. RecycleView
+
+    }
+}
